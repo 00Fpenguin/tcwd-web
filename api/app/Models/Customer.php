361a2a996dbd27b230b098034598de5adbd4404a
Anjo Tadena <tadena.anjo@gmail.com>
2021-01-24 18:32:20 +0800

    Add Customer model, migration, and factory

--- api/app/Models/Customer.php
+++ api/app/Models/Customer.php
@@ -7,5 +7,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    use HasFactory;
+
+    protected $fillable = [
+        'account_number',
+        'user_count',
+        'type_of_water_service',
+        'block_number',
+        'barangay',
+        'city',
+        'route',
+        'nearest_neighbor',
+        'is_active',
+        'birth_date',
+        'account_name',
+        'contact_number',
+        'civil_status',
+        'gender',
+        'spouse_name',
+        'ownership_details',
+    ];
+
+    public function user()
+    {
+        return $this->morphOne(User::class, 'typeable');
+    }
 }
---
+++ api/database/migrations/2021_01_22_130409_create_customers_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('account_number');
+            $table->string('user_count')->default(0);
+            $table->enum('type_of_water_service', [1, 2, 3]);
+            $table->string('block_number')->nullable();
+            $table->string('barangay')->nullable();
+            $table->string('city')->nullable();
+            $table->string('route')->nullable();
+            $table->string('nearest_neighbor')->nullable();
+            $table->boolean('is_active')->default(User::INACTIVE);
+            $table->date('birth_date');
+            $table->string('account_name')->nullable();
+            $table->string('contact_number')->nullable();
+            $table->enum('civil_status', [User::SINGLE, User::MARRIED, User::WIDOWED]);
+            $table->enum('gender', [User::MALE, User::FEMALE]);
+            $table->string('spouse_name')->nullable();
+            $table->enum('ownership_details', [1, 2])->nullable(); // owned, unowned
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
