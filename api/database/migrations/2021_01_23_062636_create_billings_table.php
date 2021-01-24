f94498f448e5044e58e69fdcd9e13adad5b15d65
Anjo Tadena <tadena.anjo@gmail.com>
2021-01-24 18:33:30 +0800

    Add Bill model, migration, and factory

---
+++ api/database/migrations/2021_01_23_062636_create_billings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBillingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('billings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('customer_id');
+            $table->double('amount');
+            $table->integer('vat');
+            $table->date('due_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('billings');
+    }
+}
