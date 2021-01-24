e9c9ebb309cb4aa1b8505a2fb0886d7982964a5d
Anjo Tadena <tadena.anjo@gmail.com>
2021-01-24 18:34:06 +0800

    Add payment migration

---
+++ api/database/migrations/2021_01_23_062946_create_payments_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->string('reference_number');
+            $table->double('fee');
+            $table->double('amount_paid');
+
+            $table->foreignId('payment_method_id');
+            $table->foreignId('customer_id');
+
+            $table->timestamps();
+
+            $table->foreign('payment_method_id')->references('id')->on('payment_methods');
+            $table->foreign('customer_id')->references('id')->on('customers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
